e1c86ba69b006140d842da195d6cb020a58f2e33
barbibrussa <barbibrussa@gmail.com>
2021-06-17 21:03:59 -0300

    Remove hello world component from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,10 @@
 <template>
-  <hello-world />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default Vue.extend({
   name: 'Home',
-
-  components: {
-    HelloWorld,
-  },
 });
 </script>
